0725c6a528bec49cb3b4913a313ac3f5dfa2507e
peterKhanh <tkhn2020@gmail.com>
2025-06-05 16:38:43 +0700

    Add GateWay Service

--- Cloud-AccountService/src/main/java/cloud/NotificationService.java
+++ Cloud-AccountService/src/main/java/cloud/NotificationService.java
@@ -6,10 +6,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import cloud.entity.MailInfo;
-import cloud.entity.Statistic;
 
 
-@FeignClient(name="notification-service", url = "http://localhost:9083/api", fallback = NotificationServiceIplm.class)
+@FeignClient(name="notification-service",  fallback = NotificationServiceIplm.class)
 public interface NotificationService {
 
     @PostMapping("/send-notification")
--- Cloud-AccountService/src/main/java/cloud/StatisticService.java
+++ Cloud-AccountService/src/main/java/cloud/StatisticService.java
@@ -1,17 +1,14 @@
 package cloud;
 
-import org.hibernate.internal.build.AllowSysOut;
-import org.slf4j.LoggerFactory;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import java.util.logging.*;
 
 import cloud.entity.Statistic;
 
 
-@FeignClient(name="statistic-service", url = "http://localhost:9082", fallback = StatisticServiceIplm.class)
+@FeignClient(name="statistic-service", fallback = StatisticServiceIplm.class)
 public interface StatisticService {
 	@PostMapping("/add")
 	void save(@RequestBody Statistic statistic); 
